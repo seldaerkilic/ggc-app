aa715b241f2513b4718c7ca5e73ee4f3ef3601cc
Selda Erkilic <seldaerkilic@gmail.com>
2023-06-18 14:46:14 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Carousel for banner
+
+// keep the element in a variable
+// add a function to toggle class for visibility of the banner image
+// use setInterval to change the image visibility
+
+const bannerItem = document.getElementById('bannerItem');
+
+function toggleClass(){
+    bannerItem.classList.toggle('hideItem');
+}
+
+setInterval(toggleClass, 3000);
+
+// Media queries
+
+// Netlify and GitHub
+// Test the site
+
+// Hamburger menu for media queries
+
+// Contact page with fromspree
+
+
+
+
